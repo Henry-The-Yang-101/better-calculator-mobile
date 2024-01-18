eb07b136dad97e9cfdba880b38556dbcfcbeb5b4
Henry Yang <henryyang@v1040-wn-rt-b-78-183.campus-dynamic.uwaterloo.ca>
2024-01-17 19:50:48 -0500

    progress so far

---
+++ App.js
@@ -0,0 +1,207 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
+import { StyleSheet, FlatList, Text, Button, Alert, View, SafeAreaView, Image, Pressable } from 'react-native';
+
+// RN Code
+const Item = ({ item }) => {
+  return <View style={styles.item}>{item.icon}</View>;
+};
+
+export default function App() {
+  const AddToDisplayText = (displayText, character) => {
+    if (displayText === "0") {
+      if (!(character.charAt(0) >= '0' && character.charAt(0) <= '9')) {
+        if (character.charAt(0) == '.') {
+          setIsDecimal(true);
+        }
+        return "0" + character;
+      } else {
+        return character;
+      }
+    }
+
+    if (character.charAt(0) >= '0' && character.charAt(0) <= '9') {
+
+    }
+    if (character.charAt(0) == '.') {
+      if (displayText.charAt(displayText.length - 1) < '0' || displayText.charAt(displayText.length - 1) > '9' || isDecimal) {
+        return displayText;
+      } else {
+        setIsDecimal(true);
+      }
+    }
+
+
+    return displayText + character;
+  }
+
+  const ClearDisplay = () => {
+    setDisplayText("0");
+    setIsDecimal(false);
+  }
+
+  const Calculate = () => {
+  }
+
+  var [displayText, setDisplayText] = useState("0");
+  var [isDecimal, setIsDecimal] = useState(false);
+  return (
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: "black",
+      // alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <View style={{
+        backgroundColor: "black",
+        flex: 1,
+        bottom: 0,
+        right: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        marginHorizontal: 15,
+      }}>
+
+        <Text style={styles.displayText}>
+          {displayText}
+        </Text>
+      </View>
+
+      <View style={{
+        backgroundColor: "black",
+        flex: 2
+      }}>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Pressable onPress={() => ClearDisplay()} style={styles.cell3} android_ripple={{ color: 'white' }}>
+              <Text style={styles.cellText1}>C</Text>
+            </Pressable>
+            <Pressable onPress={() => console.log("gyatt")} style={styles.cell3} android_ripple={{ color: 'white' }}>
+              <Text style={styles.cellText1}>()</Text>
+            </Pressable>
+            <Pressable onPress={() => console.log("gyatt")} style={styles.cell3} android_ripple={{ color: 'white' }}>
+              <Text style={styles.cellText1}>%</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '÷'))} style={styles.cell2} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText1}>÷</Text>
+            </Pressable>
+          </View>
+          <View style={styles.row}>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '7'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>7</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '8'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>8</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '9'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>9</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '×'))} style={styles.cell2} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText1}>×</Text>
+            </Pressable>
+          </View>
+          <View style={styles.row}>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '4'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>4</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '5'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>5</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '6'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>6</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '-'))} style={styles.cell2} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText1}>-</Text>
+            </Pressable>
+          </View>
+          <View style={styles.row}>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '1'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>1</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '2'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>2</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '3'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>3</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '+'))} style={styles.cell2} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText1}>+</Text>
+            </Pressable>
+          </View>
+          <View style={styles.row}>
+            <Pressable onPress={() => console.log("gyatt")} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>+/-</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '0'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>0</Text>
+            </Pressable>
+            <Pressable onPress={() => setDisplayText(AddToDisplayText(displayText, '.'))} style={styles.cell1} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText2}>.</Text>
+            </Pressable>
+            <Pressable onPress={() => Calculate()} style={styles.cell2} android_ripple={{ color: 'light grey' }}>
+              <Text style={styles.cellText1}>=</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+
+      <StatusBar style="light" />
+    </SafeAreaView>
+
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  displayText: {
+    color: "white",
+    fontSize: 60,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  cell1: {
+    width: 96,
+    height: 108,
+    margin: 0,
+    backgroundColor: "#505050",
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cell2: {
+    width: 96,
+    height: 108,
+    margin: 0,
+    backgroundColor: "#FF9500",
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cell3: {
+    width: 96,
+    height: 108,
+    margin: 0,
+    backgroundColor: "#D4D4D2",
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cellText1: {
+    color: 'black',
+    fontSize: 25,
+  },
+  cellText2: {
+    color: 'white',
+    fontSize: 25,
+  },
+});
